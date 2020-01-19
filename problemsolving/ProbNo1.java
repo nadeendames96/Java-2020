dabb15e57b5874153f4a8a96a241bf038484acc6
Nadeen Omar Dames <nadeendames96@users.noreply.github.com>
2020-01-19 17:21:34 +0200

    Add files via upload

---
+++ problemsolving/ProbNo1.java
@@ -0,0 +1,24 @@
+package problemsolving;
+
+import java.util.Scanner;
+
+public class ProbNo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ 
+	Scanner in=new Scanner(System.in);
+	int num1,num2,num3;
+	num1=in.nextInt();
+	num2=in.nextInt();
+	num3=in.nextInt();
+	
+	System.out.println(num1);
+	System.out.println(num2);
+	System.out.println(num3);
+	
+		    
+		
+	}
+
+}
